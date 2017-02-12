8845457fa5d89de771eab6f049eba025a550dbf7
Hector Ceja <cejagomezhector@gmail.com>
2017-02-12 12:12:00 -0700

    se agregaron imagenes y se mejoraron validaciones de formato

--- js/funciones.js
+++ js/funciones.js
@@ -1,8 +1,9 @@
 //declaracion de variables
 var muestroHora = document.getElementById('muestro-hora');
+var botonMusica = document.getElementById('botonMusica')
 
 
-//funtions
+//functions
 function mueveReloj(){
     momentoActual = new Date()
 
@@ -15,23 +16,31 @@ function mueveReloj(){
     var segundo = momentoActual.getSeconds();
     var segundoConCero = (segundo<=9)? "0"+segundo : ""+segundo;
 
-    if(hora<=12){
+
+    if(hora==0){
+      horaImprimible = 12 + " : " + minutoConCero + " : " + segundoConCero+ " AM";
+    }else if(hora<12){
       horaImprimible = horaConCero + " : " + minutoConCero + " : " + segundoConCero+ " AM";
     }else{
       for(var horaPM = 1; horaPM <= 12; horaPM++ ){
         if( ( horaConCero-horaPM ) == 12 ){
           var horaPMConCero = (horaPM<=9)? "0"+horaPM : ""+horaPM;
           horaImprimible = horaPMConCero + " : " + minutoConCero + " : " + segundoConCero+ " PM";
+        }else if (horaConCero-horaPM==0){
+          horaImprimible = 12 + " : " + minutoConCero + " : " + segundoConCero+ " PM";
         }
+
       }
     }
 
     muestroHora.innerHTML = "<h1>"+horaImprimible+"</h1>";
 }
 
+
 function recargaHora(){
   setInterval(mueveReloj,1000);
 }
+
 
 function musica(){
 	var audio = document.createElement("audio");
@@ -42,7 +51,9 @@ function musica(){
 function cargaDocumento(){
   recargaHora();
   musica();
+
 }
 
-//aplicacion del evento
+
+//aplicacion de los eventos
 window.addEventListener("load",cargaDocumento);
