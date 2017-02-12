6f1eab07e7386f4eb1e4b2423b85a5af05e37a27
Hector Ceja <cejagomezhector@gmail.com>
2017-02-11 21:18:32 -0700

    se puso imagen fondo

--- js/funciones.js
+++ js/funciones.js
@@ -3,29 +3,6 @@ var muestroHora = document.getElementById('muestro-hora');
 
 
 //funtions
-/*function reloj(){
-	var fechaReloj = new Date();
-	var horaReloj = fechaReloj.getHours();
-	var horaCero = (horaReloj<=9)?"0"+horaReloj:""+horaReloj;
-
-	var minutoReloj = fechaReloj.getMinutes();
-	var minutoCero = (minutoReloj<=9)?"0"+minutoReloj:""+minutoReloj;
-
-	var segundoReloj = fechaReloj.getSeconds();
-	var segundoCero = (segundoReloj<=9)?"0"+segundoReloj:""+segundoReloj;
-
-	if (horaCero>12){
-		for(var i=0;i<=11;i++){
-			muestroHora.innerHTML = "<h1>"+i+":"+minutoCero+":"+segundoCero+" PM</h1>";
-		}
-	}else{
-		if(horaCero==0){
-			muestroHora.innerHTML = "<h1>"+12+":"+minutoCero+":"+segundoCero+" AM</h1>";
-		}else{
-		muestroHora.innerHTML = "<h1>"+horaCero+":"+minutoCero+":"+segundoCero+" AM</h1>";
-		}
-	}
-}*/
 function mueveReloj(){
     momentoActual = new Date()
 
